dba84c642a5dd243af9a0fb6529052bc881543f3
dahlak76 <88516807+Dahlak76@users.noreply.github.com>
2022-09-09 18:40:00 -0500

    connected to DB/ rendering to browser

--- server/db/index.js
+++ server/db/index.js
@@ -1,1 +1,11 @@
-/// optional database server file
+const mongoose = require('mongoose');
+
+const password = process.env.DB_PASS;
+const username = process.env.DB_USER;
+const mongoUri = `mongodb+srv://${username}:${password}@newhome.ha7ybpt.mongodb.net/?retryWrites=true&w=majority`;
+
+const db = mongoose.connect(mongoUri, () => {
+  console.log('connected to mongoose');
+});
+
+module.exports = db;
