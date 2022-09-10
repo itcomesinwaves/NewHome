845404a0b6f5aaacb0ebdb1eb4b999b0282f57c5
dahlak76 <88516807+Dahlak76@users.noreply.github.com>
2022-09-10 11:07:53 -0500

    testing es lint again

--- server/index.js
+++ server/index.js
@@ -1,19 +1,18 @@
 require('dotenv').config();
 
 const express = require('express');
-const axios = require('axios');
-const db = require('./db/index.js');
-const app = express();
+// const axios = require('axios');
 const path = require('path');
+require('./db/index.js');
+const app = express();
 const PORT = 8080;
-const url = `localhost`;
+const url = 'localhost';
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.resolve('client', 'dist')));
 app.use(express.json());
 
-//const index = '../client/dist/index.html'
-var num = 4;
+// const index = '../client/dist/index.html'
 app.get('/', (req, res) => {
   res.render('index');
 });
