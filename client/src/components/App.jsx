2f71e08b38a294efc040e3da47a96b4953a9d5fc
LoganVoisin <flyflameking@gmail.com>
2022-09-10 14:34:01 -0500

    fix: ReactRoutes now switches view

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,8 +4,11 @@ import { Link } from 'react-router-dom';
 const App = () => {
   return (
     <div>
+    <nav>
+      <Link to='/'>App</Link>
+      <Link to='/profile'>Profile</Link>
+    </nav>
       Howdy
-        <Link to='/profile'>Profile</Link>
     </div>
     
   )
