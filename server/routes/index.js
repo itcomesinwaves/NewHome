1cefbb123034836f788ce4ad7586819f184a8a14
dahlak76 <88516807+Dahlak76@users.noreply.github.com>
2022-09-10 20:07:03 -0500

    initial route setup

---
+++ server/routes/index.js
@@ -0,0 +1,4 @@
+module.exports.user = require('./user.js');
+module.exports.pets = require('./pets.js');
+module.exports.feed = require('./feed.js');
+module.exports.api = require('./api.js');
---
+++ server/routes/user.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const User = require('../db/models/User.js');
+
+const user = express.Router();
+
+module.exports = user;
