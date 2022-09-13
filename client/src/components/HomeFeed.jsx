e1e260e32fbb81bb3380b3946e0504813f2e4248
itcomesinwaves <royce.taylor789@gmail.com>
2022-09-12 19:13:05 -0500

    added routing to homefeed

--- client/src/components/HomeFeed.jsx
+++ client/src/components/HomeFeed.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function HomeFeed() {
-  
+  return (
+    <>
+      <h3>Welcome home Darth Maul ....</h3>
+    </>
+  )
 }
 
 export default HomeFeed;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './components/App.jsx';
 import Profile from './components/Profile.jsx';
 import Login from './components/Login.jsx';
+import HomeFeed from './components/HomeFeed.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('app'));
 root.render(
@@ -12,6 +13,7 @@ root.render(
       <Route path="/" element={<App />} />
       <Route path="profile" element={<Profile />} />
       <Route path="login" element={<Login />} /> 
+      <Route path="home" element={<HomeFeed />} />
     </Routes>
   </BrowserRouter>,
 );
