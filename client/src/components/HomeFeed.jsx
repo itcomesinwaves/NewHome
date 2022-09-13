95343db7ff161b986a8041f5ec8f1372c007eb4c
itcomesinwaves <royce.taylor789@gmail.com>
2022-09-13 10:39:56 -0500

    fixing merge

---
+++ client/src/components/HomeFeed.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import axios from 'axios';
+import { useState } from 'react';
+
+function HomeFeed() {
+  const [animals, getAnimals] = useState([]);
+
+  const getAllAnimals = function() {
+    axios.get(('/api')
+      .then(() => {
+        setAnimals()
+      })
+      .catch((err) => {
+        console.error(err);
+      }))
+  }
+
+useEffect(getAllAnimals);
+
+
+  return (
+    <>
+      <h3>Welcome home Darth Maul ....</h3>
+    </>
+  )
+}
+export default HomeFeed;
