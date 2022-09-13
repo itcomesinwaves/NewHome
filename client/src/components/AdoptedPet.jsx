895a88d1d02be154f872b5380d00187564b0f5a4
Logan-Ward <ward_logan@outlook.com>
2022-09-13 14:51:26 -0500

    feat: Basic stylings for adoption story form

--- client/src/components/AdoptedPet.jsx
+++ client/src/components/AdoptedPet.jsx
@@ -1,16 +1,35 @@
 import React from 'react';
-import TextField from '@material-ui/core/TextField';
+import { Box, TextField, Button } from '@material-ui/core';
 
 function AdoptedPet() {
   return (
-    <div>
-      <TextField label="Title" variant="standard" />
+    <Box
+      component="form"
+      sx={{
+			  '& .MuiTextField-root': { m: 16, width: '30ch' },
+			  display: 'inline-block',
+      }}
+    >
+      <TextField label="Title" variant="standard" multiline maxRows={2} />
+      <br />
       <TextField
-        id="standard-multiline-basic"
         label="Story"
         variant="standard"
+        multiline
+        maxRows={5}
+        minRows={3}
+        mb={2}
       />
-    </div>
+      <br />
+      <br />
+      <Button
+        variant="contained"
+        mt={2}
+        sx={{ display: 'inline-block', margin: 'auto' }}
+      >
+        Submit
+      </Button>
+    </Box>
   );
 }
 
