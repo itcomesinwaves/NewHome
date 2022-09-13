347577f61ee8cc451c41d150d102385069bc6f4c
Logan-Ward <ward_logan@outlook.com>
2022-09-13 15:09:40 -0500

    feat: React Hooks for adoption story inputs

--- client/src/components/AdoptedPet.jsx
+++ client/src/components/AdoptedPet.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, TextField, Button } from '@material-ui/core';
 
 function AdoptedPet() {
+  const [story, setStory] = useState('');
+  const [title, setTitle] = useState('');
+
   return (
     <Box
       component="form"
@@ -9,16 +12,29 @@ function AdoptedPet() {
 			  '& .MuiTextField-root': { m: 16, width: '30ch' },
 			  display: 'inline-block',
       }}
+      onSubmit={(e) => {
+			  e.preventDefault();
+			  console.log(`Title: ${title} \n Story: ${story}`);
+      }}
     >
-      <TextField label="Title" variant="standard" multiline maxRows={2} />
+      <TextField
+        label="Title"
+        variant="standard"
+        multiline
+        maxRows={2}
+        id="Title"
+        onChange={(e) => setTitle(e.target.value)}
+      />
       <br />
       <TextField
+        id="Story"
         label="Story"
         variant="standard"
         multiline
         maxRows={5}
         minRows={3}
         mb={2}
+        onChange={(e) => setStory(e.target.value)}
       />
       <br />
       <br />
@@ -26,6 +42,7 @@ function AdoptedPet() {
         variant="contained"
         mt={2}
         sx={{ display: 'inline-block', margin: 'auto' }}
+        type="submit"
       >
         Submit
       </Button>
