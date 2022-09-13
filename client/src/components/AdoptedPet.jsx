1cc940ae556b0fe8fa6b6fa01cff31be116fa103
Logan-Ward <ward_logan@outlook.com>
2022-09-13 10:51:15 -0500

    feat: input fields rendering to adoptedPets page

--- client/src/components/AdoptedPet.jsx
+++ client/src/components/AdoptedPet.jsx
@@ -2,7 +2,16 @@ import React from 'react';
 import TextField from '@material-ui/core/TextField';
 
 function AdoptedPet() {
-  return <TextField variant="" />;
+  return (
+    <div>
+      <TextField label="Title" variant="standard" />
+      <TextField
+        id="standard-multiline-basic"
+        label="Story"
+        variant="standard"
+      />
+    </div>
+  );
 }
 
 export default AdoptedPet;
