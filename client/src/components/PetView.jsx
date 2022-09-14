6432405cb77f687f7c6af88184fc968f997502fe
erincwilliams0 <erin.cwilliams0@gmail.com>
2022-09-14 11:09:10 -0500

    added previous code in from component, testing prettier

---
+++ client/src/components/PetView.jsx
@@ -0,0 +1,102 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+// example pet object to render for mvp
+const pet = {
+  species: 'Cat',
+  breed: 'Calico',
+  gender: 'Female',
+  size: 'Medium',
+  coat: 'short',
+  name: 'Bonnie',
+  age: 'Adult',
+  temperament: ['Friendly', 'Gentle', 'Affectionate'],
+  shelterInfo: {
+    email: 'adoptions@centralfallsanimals.org',
+    address: {
+      city: 'Central Falls',
+      state: 'RI',
+      postcode: '02863',
+    },
+  },
+  adopted: false,
+  image:
+		'https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/57334144/1/?bust=1663026743&width=300',
+  userId: 'myID',
+};
+
+function PetView() {
+  // isloggedin
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  // function to save/follow a pet
+  const handleSavePet = (e) => {
+    // check if user is logged in, and button id;
+    if (!loggedIn) {
+      window.alert('Please sign up/login');
+    } else if (e.target.id === 'save') {
+      // axios request for favoriting a pet
+      console.log('testing save request');
+    } else {
+      // axios request for following a pet story
+      console.log('testing follow request');
+    }
+    // alert user that the pet has been saved
+  };
+
+  // conditional rendering based on pet adoption status
+  const onAdoptionStatus = () => {
+    // if pet === adopted render follow button, render adoption stories
+    if (pet.adopted) {
+      // render follow button and pet stories
+      return (
+        <input
+          type="button"
+          id="follow"
+          value="follow"
+          onClick={(e) => handleSavePet(e)}
+        />
+      );
+    }
+    // render save button
+    return (
+      <input
+        type="button"
+        id="save"
+        value="save"
+        onClick={(e) => handleSavePet(e)}
+      />
+    );
+  };
+
+  return (
+    <div>
+      <h1>
+        {pet.name}
+        {' '}
+        says hello
+      </h1>
+      <img src={pet.image} alt="img here" />
+      <h3>About me:</h3>
+      <p>
+        Species:
+        {pet.species}
+      </p>
+      <p>
+        Breed:
+        {pet.breed}
+      </p>
+      <p>
+        Age:
+        {pet.age}
+      </p>
+      <h3>I&apos;m</h3>
+      <ul>
+        {pet.temperament.map((tag) => <li key={`${pet.id}${pet.name}`}>{tag}</li>)}
+      </ul>
+      {onAdoptionStatus()}
+    </div>
+  );
+}
+
+export default PetView;
