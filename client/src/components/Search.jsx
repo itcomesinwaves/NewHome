8fcda0673a47f7a90927a57eb12cb6bd80512c81
LoganVoisin <flyflameking@gmail.com>
2022-09-14 11:56:04 -0500

    add: matUI to search

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,6 +1,13 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { Button } from '@material-ui/core';
+import {
+  Button,
+  Select,
+  MenuItem,
+  InputLabel,
+  Box,
+  TextField,
+} from '@material-ui/core';
 
 function Search() {
   const [breed, setVal] = useState(() => '');
@@ -63,43 +70,71 @@ function Search() {
 
   return (
     <div id="search">
-      <form onSubmit={submit}>
-        Breed:
-        <input type="text" value={breed} onChange={breedUpdate} />
-        <select value={hairLength} onChange={hairUpdate}>
-          <option value="">Hair length</option>
-          <option value="short">Short</option>
-          <option value="medium">Medium</option>
-          <option value="long">Long</option>
-        </select>
-        <select value={species} onChange={speciesUpdate}>
-          <option value="">Species</option>
-          <option value="cat">Cat</option>
-          <option value="dog">Dog</option>
-        </select>
-        <select value={age} onChange={ageUpdate}>
-          <option value="">Age</option>
-          <option value="baby">Baby</option>
-          <option value="young">Young</option>
-          <option value="adult">Adult</option>
-          <option value="senior">Senior</option>
-        </select>
-        <select value={gender} onChange={genderUpdate}>
-          <option value="">Gender</option>
-          <option value="male">Male</option>
-          <option value="female">Female</option>
-        </select>
-        <select value={size} onChange={sizeUpdate}>
-          <option value="">Size</option>
-          <option value="small">Small</option>
-          <option value="medium">Medium</option>
-          <option value="large">Large</option>
-          <option value="xlarge">X-Large</option>
-        </select>
-        <Button variant="contained" type="submit">
+      <Box
+        component="form"
+        onSubmit={submit}
+        sx={{
+				  display: 'inline-block',
+				  maxWidth: '500px',
+				  minWidth: '280px',
+				  '& .MuiTextField-root': { width: '280px' },
+        }}
+      >
+        <TextField
+          label="breed"
+          type="text"
+          value={breed}
+          onChange={breedUpdate}
+        />
+        <InputLabel id="Hair-Length">Hair Length</InputLabel>
+        <Select
+          labelId="Hair-Length"
+          label="Hair length"
+          value={hairLength}
+          onChange={hairUpdate}
+        >
+          <MenuItem value="short">Short</MenuItem>
+          <MenuItem value="medium">Medium</MenuItem>
+          <MenuItem value="long">Long</MenuItem>
+        </Select>
+        <InputLabel id="Species">Species</InputLabel>
+        <Select
+          labelId="Species"
+          label="Species"
+          value={species}
+          onChange={speciesUpdate}
+        >
+          <MenuItem value="cat">Cat</MenuItem>
+          <MenuItem value="dog">Dog</MenuItem>
+        </Select>
+        <InputLabel id="Age">Age</InputLabel>
+        <Select labelId="Age" label="Age" value={age} onChange={ageUpdate}>
+          <MenuItem value="baby">Baby</MenuItem>
+          <MenuItem value="young">Young</MenuItem>
+          <MenuItem value="adult">Adult</MenuItem>
+          <MenuItem value="senior">Senior</MenuItem>
+        </Select>
+        <InputLabel id="Gender">Gender</InputLabel>
+        <Select
+          labelId="Gender"
+          label="Gender"
+          value={gender}
+          onChange={genderUpdate}
+        >
+          <MenuItem value="male">Male</MenuItem>
+          <MenuItem value="female">Female</MenuItem>
+        </Select>
+        <InputLabel id="Size">Size</InputLabel>
+        <Select labelId="Size" label="Size" value={size} onChange={sizeUpdate}>
+          <MenuItem value="small">Small</MenuItem>
+          <MenuItem value="medium">Medium</MenuItem>
+          <MenuItem value="large">Large</MenuItem>
+          <MenuItem value="xlarge">X-Large</MenuItem>
+        </Select>
+        <Button variant="contained" type="submit" color="primary">
           Submit
         </Button>
-      </form>
+      </Box>
     </div>
   );
 }
