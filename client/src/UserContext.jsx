00e4cebc69c3f4a504e88f83c428241bc79697f8
erincwilliams0 <erin.cwilliams0@gmail.com>
2022-09-16 14:12:58 -0500

    UserContext passing props to app

---
+++ client/src/UserContext.jsx
@@ -0,0 +1,33 @@
+import React, { createContext, useState, useMemo } from 'react';
+import PropTypes from 'prop-types';
+
+// context element
+const UserContext = createContext();
+
+function UserContextProvider({ children }) {
+  // useState to set variables and updateVariables
+  const [user, setUser] = useState(null);
+  const [savedList, setSavedList] = useState(null);
+  const [postList, setPostList] = useState(null);
+
+  const userValue = useMemo(
+    () => ({
+      user,
+      setUser,
+      savedList,
+      setSavedList,
+      postList,
+      setPostList,
+    }),
+    [user, savedList, postList],
+  );
+  return (
+    <UserContext.Provider value={userValue}>{children}</UserContext.Provider>
+  );
+}
+// user object and functionality to set the user
+UserContextProvider.propTypes = {
+  children: PropTypes.array.isRequired,
+};
+
+export { UserContext, UserContextProvider };
