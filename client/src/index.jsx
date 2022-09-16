6743c5ed98030933242e8a8dbf4ecb7df7182540
erincwilliams0 <erin.cwilliams0@gmail.com>
2022-09-16 13:31:01 -0500

    implemented UserContextProvider

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -9,20 +9,24 @@ import PostFeed from './components/PostFeed.jsx';
 import Search from './components/Search.jsx';
 import PostForms from './components/PostForms.jsx';
 import PetView from './components/PetView.jsx';
+import { userContext, UserContextProvider } from './UserContext.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('app'));
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route path="profile" element={<Profile />} />
-        <Route path="login" element={<Login />} />
-        <Route path="search" element={<Search />} />
-        <Route path="home" element={<AdoptionFeed />} />
-        <Route path="postfeed" element={<PostFeed />} />
-        <Route path="postforms" element={<PostForms />} />
-        <Route path="petview" element={<PetView />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>,
+  <UserContextProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="profile" element={<Profile />} />
+          <Route path="login" element={<Login />} />
+          <Route path="search" element={<Search />} />
+          <Route path="home" element={<AdoptionFeed />} />
+          <Route path="postfeed" element={<PostFeed />} />
+          <Route path="postforms" element={<PostForms />} />
+          <Route path="petview" element={<PetView />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    ,
+  </UserContextProvider>,
 );
