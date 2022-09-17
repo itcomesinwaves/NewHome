9e0f8072360e68673d879ddf8a9aafca07f970ac
erincwilliams0 <erin.cwilliams0@gmail.com>
2022-09-16 20:24:05 -0500

    feat: pseudocode for post to /savePet

--- server/routes/pet.js
+++ server/routes/pet.js
@@ -2,5 +2,18 @@ const express = require('express');
 
 const pet = express.Router();
 const Pet = require('../db/models/Pet.js');
+const SavedPet = require('../db/models/SavedPet.js');
+
+pet.post('/savePet', (req, res) => {
+  // log body provided by client
+  console.log(req.body);
+
+  // check if pet is in database
+  // save pet to db Pet collection
+  // *if found move on without saving
+  // then check saved pet documents for previous save
+  // create user/pet SavedPet
+  // if found move on/handle sending back error res
+});
 
 module.exports = pet;
