742c14702e39d07f22d1715efba6b6e2ea22db1c
dahlak76 <88516807+Dahlak76@users.noreply.github.com>
2022-09-17 12:01:42 -0500

    Merge branch 'main' of https://github.com/Sinntaxx/NewHome into profile-feature

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -77,32 +77,4 @@ function Profile() {
     </Box>
   );
 }
-// JSON.stringify(profile, null, 2)
 export default Profile;
-// async (accessToken, refreshToken, profile, done) => {
-//   //get the user data from google
-//   const newUser = {
-//     googleId: profile.id,
-//     displayName: profile.displayName,
-//     firstName: profile.name.givenName,
-//     lastName: profile.name.familyName,
-//     image: profile.photos[0].value,
-//     email: profile.emails[0].value
-//   }
-
-//   try {
-//     //find the user in our database
-//     let user = await User.findOne({ googleId: profile.id })
-
-//     if (user) {
-//       //If user present in our database.
-//       done(null, user)
-//     } else {
-//       // if user is not preset in our database save user data to database.
-//       user = await User.create(newUser)
-//       done(null, user)
-//     }
-//   } catch (err) {
-//     console.error(err)
-//   }
-// }
